0e09c05723de1defdb3543cc4c6a815f31b9289e
ngduc <nducus@yahoo.com>
2021-07-16 03:22:31 +0100

    fix: unit tests

--- test.js
+++ test.js
@@ -3,5 +3,6 @@ const execa = require('execa');
 
 test('main', async (t) => {
   const { stdout } = await execa('./index.js', ['example.sol']);
+  console.log(stdout)
   t.is(stdout.indexOf('Success') >= 0, true);
 });
